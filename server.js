78a6642768d931e0e906e77d5181643c0f723c80
Guy Hanan <yhananguy@gmail.com>
2025-09-10 19:12:03 +0300

    Update server.js

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ import morgan from "morgan"
 import recipesRouter from "./routes/recipesRouter.js";
 
 const app = express();
+app.get('/healthz', (req,res) => res.send('ok'));
+app.head('/healthz', (req,res) => res.sendStatus(200));
 app.use(express.json());
 morgan.token("time", () => new Date().toISOString());
 app.use(morgan(":method :url :status :response-time ms - :time"));
